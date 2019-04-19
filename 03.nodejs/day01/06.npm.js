8154f52d680bf65b2c0b77b5a6053a7d3d51bdc5
xiongjian <webjsforyou@163.com>
2019-04-19 15:00:31 +0800

    package.json和npm

---
+++ 03.nodejs/day01/06.npm.js
@@ -0,0 +1,25 @@
+/*
+  npm 包管理器，用来管理（下载、删除、发布、更新...）所有包
+  常用指令：
+    1. 下载
+      npm install / i xxx
+        node_modules/  里面包含所有通过npm下载的包
+        package-lock.json 缓存文件、里面包含之前下载包的信息（版本号、下载地址、hash值），目的为了下一次在下载重复的包时速度更快
+        将被下载的包 往package.json添加为生产依赖
+      等价于 npm i xxx --save / -S 下载并添加到生产依赖中
+
+      npm i xxx --save-dev / -D 下载并添加到开发依赖中
+
+      npm i 下载当前目录下package.json中所有依赖包
+    2. 删除
+      npm remove xxx  删除node_modules中的包，和package.json、package-lock.json中依赖关系
+      npm uninstall xxx
+    3. 扩展
+      npm init -y 初始化包描述文件（包含基本配置） package.json
+
+    注意：
+      1. 保证当前包中有package.json文件
+      2. 保证下载时是在当前package.json文件的目录下下载
+
+    在cmd中退出操作：ctrl + c
+ */
