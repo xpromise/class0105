dca4a95e4939185abe99a2414d585112fe1ee710
xiongjian <webjsforyou@163.com>
2019-04-22 15:17:02 +0800

    扩展了http注释

--- 03.nodejs/day03/04.测试http协议.js
+++ 03.nodejs/day03/04.测试http协议.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+/*
+  http协议：
+    1. 规定互联网中客户端和服务器之间的通信的规则
+    2. 超文本超文本传输协议，基于TCP/IP协议位于应用层的协议
+    3. 协议规定的具体内容：我们称之为报文：请求报文和响应报文
+ */
 
 app.get('/login', (req, res) => {
 
@@ -9,4 +15,4 @@ app.get('/login', (req, res) => {
 app.listen(4000, (err) => {
   if (!err) console.log('服务器启动成功了~');
   else console.log(err);
-})
+});
