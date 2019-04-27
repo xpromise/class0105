6baa3b3d2ffb56f18311b29f8c1a7bf8d2006838
xiongjian <webjsforyou@163.com>
2019-04-27 09:54:51 +0800

    ajax的基本使用

---
+++ 03.nodejs/day07/01.ajax.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// 我们需要通过开发者服务器打开页面，这样发送ajax请求才不会报错
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  console.log(req.query);
+
+  res.send('这是服务器返回的响应~');
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  res.send('这是post服务器返回的响应~');
+})
+
+app.listen(3000, (err) => {
+  if (!err) console.log('服务器启动成功了~');
+  else console.log(err);
+})
