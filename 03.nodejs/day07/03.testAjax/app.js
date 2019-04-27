996ce8d5a7be2c4b459e3b2866a0ae92d3ca4f2f
xiongjian <webjsforyou@163.com>
2019-04-27 15:56:50 +0800

    ajax的练习-服务器部分

---
+++ 03.nodejs/day07/03.testAjax/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const db = require('./db');
+const Cities = require('./models/cities');
+
+const app = express();
+
+db
+  .then(() => {
+    // 获取省份数据的路由
+    app.get('/province', async (req, res) => {
+      // 需要将所有省份数据返回  {level: 1}
+      // 需要不要请求参数？ 不需要：因为参数是固定不变的
+      // 投影 {name: 1, province: 1, _id: 0} 过滤掉不想要的数据
+      // 操作不管成功还是失败，都必须要返回响应（成功响应、失败响应）
+      try { // 专门用来处理异常错误
+        // try 里面放可能出错的代码，一旦代码出错，立即中断try中代码的执行，直接进入到catch
+        const result = await Cities.find({level: 1}, {name: 1, province: 1, _id: 0});
+        // 返回成功的响应给前端
+        res.json({code: 0, data: result});
+      } catch (e) {
+        // catch就用来处理错误
+        console.log(e);
+        // 返回失败的错误给前端
+        res.json({code: 1, err: e});
+      }
+    });
+    // 获取城市数据的路由
+    app.get('/city', async (req, res) => {
+      try {
+        // 查询条件： {province: xxx, level: 2}
+        // province 应该是请求参数
+        const { province } = req.query;
+
+        const result = await Cities.find({province, level: 2}, {name: 1, city: 1, _id: 0});
+        // 返回成功的响应
+        res.json({code: 0, data: result});
+      } catch (e) {
+        // 返回失败的响应
+        res.json({code: 1, err: e});
+      }
+
+    });
+    // 获取区县数据的路由
+    app.get('/county', (req, res) => {
+
+    });
+  })
+  .catch(() => {
+    console.log('数据库连接失败~');
+  })
+
+
+
+app.listen(3000, (err) => {
+  if (!err) console.log('服务器启动成功了~');
+  else console.log(err);
+});
---
+++ 03.nodejs/day07/03.testAjax/db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/ajax', { useNewUrlParser: true, useCreateIndex: true });
+
+module.exports = new Promise((resolve, reject) => {
+  mongoose.connection.once('open', (err) => {
+    if (!err) {
+      console.log('数据库连接成功了~');
+      resolve();
+    } else {
+      console.log(err);
+      reject();
+    }
+  })
+});
