96b32a5939386e8e5002964c0afee956d213eec6
xiongjian <webjsforyou@163.com>
2019-04-30 17:07:49 +0800

    koa源码

--- 06.koa/app.js
+++ 06.koa/app.js
@@ -2,16 +2,28 @@ const Koa = require('./my-koa/application');
 
 const app = new Koa();
 
-app.use((req, res, next) => {
+app.use((context, next) => {
   console.log('中间件执行了11~');
+  next();
+  /*
+    dispatch(1)
+      -->
+        ((req, res, next) => {
+          console.log('中间件执行了22~');
+          res.end('success');
+          next();
+        })()
+   */
+});
+
+app.use((context, next) => {
+  console.log('中间件执行了22~');
+  // res.end('success');
+  context.body = {name: 'jack'};
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('中间件执行了22~');
-  res.end('success');
-  next();
-});
+
 
 app.listen(3000, (err) => {
   if (!err) console.log('服务器启动成功了~');
--- 06.koa/my-koa/application.js
+++ 06.koa/my-koa/application.js
@@ -27,17 +27,39 @@ module.exports = class Application {
     const handleRequest = (req, res) => {
       // 处理请求，返回响应 --> 中间件函数需要在这调用
       // this.middleware.forEach((fn) => fn(req, res));
-      fn(req, res);
+      // fn(req, res);
+      // 创建ctx对象
+      const context = this.createContext(req, res);
+      this.handleRequest(context, fn);
     };
 
     return handleRequest;
+  }
+
+  // 处理请求：执行处理请求的中间件函数
+  handleRequest(context, middlewareFn) {
+    // 执行处理请求的中间件函数
+    // 执行返回响应的方法（等所有中间件函数执行完毕后才允许返回响应）
+    const handleResponse = () => {
+      // 来到这里，所有中间件函数执行完毕了，返回响应
+      respond(context);
+    }
+
+    middlewareFn(context).then(handleResponse);
+  }
+
+  createContext(req, res) {
+    const context = {};
+    context.req = req;
+    context.res = res;
+    return context;
   }
 
 };
 
 // 高阶函数：执行函数返回另外一个函数
 function compose(middleware) {
-  return (req, res) => {
+  return (context) => {
     return dispatch(0);
     function dispatch(i) {
       // 取出中间件函数
@@ -45,10 +67,22 @@ function compose(middleware) {
       // 如果fn不存在，说明它的值是undefined, 说明执行到头，没有其他中间件函数可以执行了
       if (!fn) return Promise.resolve();
       // dispatch.bind(null, i + 1) 就是next方法
-      return Promise.resolve(fn(req, res, dispatch.bind(null, i + 1)));
+      return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
     }
   }
 }
+
+// 返回响应的方法
+function respond(context) {
+  // 根据响应内容类型，返回不同的响应
+  let body = context.body;
+  let res = context.res;
+
+  if ('string' == typeof body) return res.end(body);
+  if ('object' == typeof body) return res.end(JSON.stringify(body));
+  res.end(body);
+}
+
 
 /*
   compose([])()  -->  dispatch(0)  fn(req, res, dispatch.bind(null, 1));
